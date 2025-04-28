4ad5ff4ebeef8d83ce25f1b093867672231e9421
Colombo <colombo.carlos@gmail.com>
2025-04-27 21:58:14 -0600

    Creating textnode

---
+++ src/textnode.py
@@ -0,0 +1,25 @@
+from enum import Enum
+
+class TextType(Enum):
+    #Enum for text types.
+    TEXT = 1
+    BOLD = 2
+    ITALIC = 3
+    CODE = 4
+    LINK = 5
+    IMAGE = 6
+    
+
+class TextNode:
+    def __init__(self, text, text_type=TextType.TEXT, url=None):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+
+    def __eq__(self, other):
+        if isinstance(other, TextNode):
+            return self.text == other.text and self.text_type == other.text_type and self.url == other.url
+        return False
+         
+    def __repr__(self):
+        return f"TextNode({self.text}, {self.text_type}, {self.url})"
