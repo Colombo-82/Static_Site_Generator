8820164844da2e205e3a42683ec5a8502cd1c382
Colombo <colombo.carlos@gmail.com>
2025-04-28 18:23:02 -0600

    Creating Tests

--- src/main.py
+++ src/main.py
@@ -1,13 +1,7 @@
 from textnode import TextNode, TextType
 
 def main():
-    # Create a TextNode with some dummy values
-    # For example, a link node with some text and a URL
     node = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
-    
-    # Print the node, which will call the __repr__ method
     print(node)
 
-# Call the main function when the script runs
-if __name__ == "__main__":
-    main()
+main()
--- src/textnode.py
+++ src/textnode.py
@@ -1,25 +1,27 @@
 from enum import Enum
 
+
 class TextType(Enum):
-    #Enum for text types.
-    TEXT = 1
-    BOLD = 2
-    ITALIC = 3
-    CODE = 4
-    LINK = 5
-    IMAGE = 6
-    
+    TEXT = "text"
+    BOLD = "bold"
+    ITALIC = "italic"
+    CODE = "code"
+    LINK = "link"
+    IMAGE = "image"
+
 
 class TextNode:
-    def __init__(self, text, text_type=TextType.TEXT, url=None):
+    def __init__(self, text, text_type, url=None):
         self.text = text
         self.text_type = text_type
         self.url = url
 
     def __eq__(self, other):
-        if isinstance(other, TextNode):
-            return self.text == other.text and self.text_type == other.text_type and self.url == other.url
-        return False
-         
+        return (
+            self.text_type == other.text_type
+            and self.text == other.text
+            and self.url == other.url
+        )
+
     def __repr__(self):
-        return f"TextNode({self.text}, {self.text_type}, {self.url})"
+        return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
